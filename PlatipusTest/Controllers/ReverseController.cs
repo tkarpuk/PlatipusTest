4051474fe2e897c24c0bebdde1f2502cb9d6aa32
Taras Karpuk <taras.karpuk@gmail.com>
2021-11-22 15:23:29 +0200

    Add Reverce controller

---
+++ PlatipusTest/Controllers/ReverseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatipusTest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReverseController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetReverseData(string data)
+        {
+            return Ok(data);
+        }
+    }
+}
